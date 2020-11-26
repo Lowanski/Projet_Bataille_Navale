30b9708101958d715d355c83ea43e90bc909af69
Elliott123789 <64467195+Elliott123789@users.noreply.github.com>
2020-11-26 17:42:41 +0100

    Ajout des attribues et du constructeur

--- src/com/ece/Navire.java
+++ src/com/ece/Navire.java
@@ -3,9 +3,17 @@ import java.awt.*;
 import java.util.Scanner;
 
 public abstract class Navire {
-    Point coord = null;
+    Point coord;
     String orientation;
+    int taille;
+    int puissanceTire;
 
+    Navire() {
+        coord = null;
+        orientation = null;
+        taille = 0;
+        puissanceTire = 0;
+    }
 
     public static class SaisieErroneeException extends Exception {
         public SaisieErroneeException(String s) {
@@ -14,77 +22,98 @@ public abstract class Navire {
     }
 
     public static void controle(String direction) throws SaisieErroneeException {
-        if (!direction.equals("D")||!direction.equals("d")||!direction.equals("G")||!direction.equals("g")||!direction.equals("H")||!direction.equals("h")||!direction.equals("B")||!direction.equals("b")||!direction.equals("X")||!direction.equals("x")) {
-            throw new SaisieErroneeException("Vous n'avez pas rentrer une direction ( g = Gauche , d = Droit , h = Haut , b = Bas");
+        if (!direction.equals("D") || !direction.equals("d") || !direction.equals("G") || !direction.equals("g")
+                || !direction.equals("H") || !direction.equals("h") || !direction.equals("B") || !direction.equals("b")
+                || !direction.equals("X") || !direction.equals("x")) {
+            throw new SaisieErroneeException(
+                    "Vous n'avez pas rentrer une direction ( g = Gauche , d = Droit , h = Haut , b = Bas");
         }
     }
 
     abstract public void tire();
 
-     public boolean bouger(){
-         String direction;
-         direction = saisirDirection(this);
-         direction.toUpperCase();
-         switch (direction){
-             case "D":{
-                 boolean yes = this.canGoRight();
-                 if (yes){
-                     this.goRight();
-                 }
-             }
+    public boolean bouger() {
+        String direction;
+        direction = saisirDirection(this);
+        direction.toUpperCase();
+        switch (direction) {
+            case "D": {
+                boolean yes = this.canGoRight();
+                if (yes) {
+                    this.goRight();
+                }
+            }
 
-             case "G":{
-                 boolean yes = this.canGoLeft();
-                 if (yes){
-                     this.goLeft();
-                 }
-             }
+            case "G": {
+                boolean yes = this.canGoLeft();
+                if (yes) {
+                    this.goLeft();
+                }
+            }
 
-             case "H":{
-                 boolean yes = this.canGoUp();
-                 if (yes){
-                     this.goUp();
-                 }
-             }
+            case "H": {
+                boolean yes = this.canGoUp();
+                if (yes) {
+                    this.goUp();
+                }
+            }
 
-             case "B":{
-                 boolean yes = this.canGoDown();
-                 if (yes){
-                     this.goDown();
-                 }
-             }
+            case "B": {
+                boolean yes = this.canGoDown();
+                if (yes) {
+                    this.goDown();
+                }
+            }
 
-             case "X":{
-                 return false;
-             }
-         }
-         return true;
-     }
-
-    protected boolean canGoRight(){
-         return true;
-    }
-    protected boolean canGoLeft(){
+            case "X": {
+                return false;
+            }
+        }
         return true;
     }
-    protected boolean canGoUp(){
+
+    protected boolean canGoRight() {
+        if (orientation == "h")//&& (coord x + taille < ?bord? )
         return true;
+        else
+        return false;
     }
-    protected boolean canGoDown(){
+
+    protected boolean canGoLeft() {
+        if (orientation == "h")//&& (coord x > ?bord? )
         return true;
+        else
+        return false;
+    }
+
+    protected boolean canGoUp() {
+        if (orientation == "v")//&& (coord y > ?bord? )
+        return true;
+        else
+        return false;
+    }
+
+    protected boolean canGoDown() {
+        if (orientation == "v")//&& (coord y + taille > ?bord? )
+        return true;
+        else
+        return false;
     }
 
     protected abstract void goDown();
+
     protected abstract void goUp();
+
     protected abstract void goLeft();
+
     protected abstract void goRight();
 
     private static String saisirDirection(Navire narive) {
         Scanner sc = new Scanner(System.in);
         boolean correct = false;
         String directionEntre;
-        do{
-            System.out.println("Saisissez une direction pour le navire : "+ narive);
+        do {
+            System.out.println("Saisissez une direction pour le navire : " + narive);
             System.out.println("Ou entrer \"X\" pour sortir du mode de déplacement");
             directionEntre = sc.nextLine();
             try {
@@ -93,7 +122,9 @@ public abstract class Navire {
             } catch (SaisieErroneeException e) {
                 System.out.println(e);
             }
-        }while(!correct);
+        } while (!correct);
+        sc.close();
         return directionEntre;
     }
+
 }
