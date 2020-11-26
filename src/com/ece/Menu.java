195594953f27341d14e2cb35f4b39b7744752d73
Lowan Steinmetz <lowan.steinmetz@edu.ece.fr>
2020-11-26 13:04:03 +0100

    On répare le bordel

--- src/com/ece/Menu.java
+++ src/com/ece/Menu.java
@@ -1,9 +1,65 @@
 package com.ece;
 
+import java.util.Scanner;
+
 public class Menu {
 
     public static void main(String[] args) {
+
+        int nombre;
+        Scanner scan = new Scanner(System.in);
+        do {
+            System.out.println("--------LA BATAILLE NAVAL--------\n");
+
+            System.out.println("1. Jouer une partie");
+            System.out.println("2. Charger une partie");
+            System.out.println("3. Aide");
+            System.out.println("4. Quitter");
+
+            System.out.println("\nSaissir une action:");
+
+            do {
+                nombre = scan.nextInt();
+                if ((nombre < 1) || (nombre > 4)) {
+                    System.out.println("le nombre n'est pas valide, ressaissir: ");
+                }
+            } while ((nombre < 1) || (nombre > 4));
+
+            switch (nombre) {
+                case 1:
+                    jouerPartie();
+                    break;
+
+                case 2:
+                    chargerPartie();
+                    break;
+                case 3:
+                    ouvrirAide();
+                    break;
+
+            }
+        } while (nombre != 4);
+        scan.close();
+        
+    }
+
+    public static void jouerPartie() {
         Grille grille = new Grille();
         grille.dessiner();
+
     }
+
+    public static void chargerPartie() {
+
+    }
+
+    public static void ouvrirAide() {
+
+
+        //trouvé ça sur internet a tester
+       /*
+        File htmlFile = new File(url);
+       Desktop.getDesktop().browse(htmlFile.toURI());      */
+    }
+
 }
