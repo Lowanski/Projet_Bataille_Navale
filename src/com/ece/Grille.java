a217a40ec9fae0628ae8d0901a40e55e129be8e7
Paul <paulchartier123@github.com>
2020-12-08 14:29:25 +0100

    Couleur console

--- src/com/ece/Grille.java
+++ src/com/ece/Grille.java
@@ -10,6 +10,81 @@ import com.ece.Navire.SaisieErroneeException;
 
 public class Grille implements Serializable {
 
+    public class ConsoleColors {
+        // Reset
+        public static final String RESET = "\033[0m";  // Text Reset
+
+        // Regular Colors
+        public static final String BLACK = "\033[0;30m";   // BLACK
+        public static final String RED = "\033[0;31m";     // RED
+        public static final String GREEN = "\033[0;32m";   // GREEN
+        public static final String YELLOW = "\033[0;33m";  // YELLOW
+        public static final String BLUE = "\033[0;34m";    // BLUE
+        public static final String PURPLE = "\033[0;35m";  // PURPLE
+        public static final String CYAN = "\033[0;36m";    // CYAN
+        public static final String WHITE = "\033[0;37m";   // WHITE
+
+        // Bold
+        public static final String BLACK_BOLD = "\033[1;30m";  // BLACK
+        public static final String RED_BOLD = "\033[1;31m";    // RED
+        public static final String GREEN_BOLD = "\033[1;32m";  // GREEN
+        public static final String YELLOW_BOLD = "\033[1;33m"; // YELLOW
+        public static final String BLUE_BOLD = "\033[1;34m";   // BLUE
+        public static final String PURPLE_BOLD = "\033[1;35m"; // PURPLE
+        public static final String CYAN_BOLD = "\033[1;36m";   // CYAN
+        public static final String WHITE_BOLD = "\033[1;37m";  // WHITE
+
+        // Underline
+        public static final String BLACK_UNDERLINED = "\033[4;30m";  // BLACK
+        public static final String RED_UNDERLINED = "\033[4;31m";    // RED
+        public static final String GREEN_UNDERLINED = "\033[4;32m";  // GREEN
+        public static final String YELLOW_UNDERLINED = "\033[4;33m"; // YELLOW
+        public static final String BLUE_UNDERLINED = "\033[4;34m";   // BLUE
+        public static final String PURPLE_UNDERLINED = "\033[4;35m"; // PURPLE
+        public static final String CYAN_UNDERLINED = "\033[4;36m";   // CYAN
+        public static final String WHITE_UNDERLINED = "\033[4;37m";  // WHITE
+
+        // Background
+        public static final String BLACK_BACKGROUND = "\033[40m";  // BLACK
+        public static final String RED_BACKGROUND = "\033[41m";    // RED
+        public static final String GREEN_BACKGROUND = "\033[42m";  // GREEN
+        public static final String YELLOW_BACKGROUND = "\033[43m"; // YELLOW
+        public static final String BLUE_BACKGROUND = "\033[44m";   // BLUE
+        public static final String PURPLE_BACKGROUND = "\033[45m"; // PURPLE
+        public static final String CYAN_BACKGROUND = "\033[46m";   // CYAN
+        public static final String WHITE_BACKGROUND = "\033[47m";  // WHITE
+
+        // High Intensity
+        public static final String BLACK_BRIGHT = "\033[0;90m";  // BLACK
+        public static final String RED_BRIGHT = "\033[0;91m";    // RED
+        public static final String GREEN_BRIGHT = "\033[0;92m";  // GREEN
+        public static final String YELLOW_BRIGHT = "\033[0;93m"; // YELLOW
+        public static final String BLUE_BRIGHT = "\033[0;94m";   // BLUE
+        public static final String PURPLE_BRIGHT = "\033[0;95m"; // PURPLE
+        public static final String CYAN_BRIGHT = "\033[0;96m";   // CYAN
+        public static final String WHITE_BRIGHT = "\033[0;97m";  // WHITE
+
+        // Bold High Intensity
+        public static final String BLACK_BOLD_BRIGHT = "\033[1;90m"; // BLACK
+        public static final String RED_BOLD_BRIGHT = "\033[1;91m";   // RED
+        public static final String GREEN_BOLD_BRIGHT = "\033[1;92m"; // GREEN
+        public static final String YELLOW_BOLD_BRIGHT = "\033[1;93m";// YELLOW
+        public static final String BLUE_BOLD_BRIGHT = "\033[1;94m";  // BLUE
+        public static final String PURPLE_BOLD_BRIGHT = "\033[1;95m";// PURPLE
+        public static final String CYAN_BOLD_BRIGHT = "\033[1;96m";  // CYAN
+        public static final String WHITE_BOLD_BRIGHT = "\033[1;97m"; // WHITE
+
+        // High Intensity backgrounds
+        public static final String BLACK_BACKGROUND_BRIGHT = "\033[0;100m";// BLACK
+        public static final String RED_BACKGROUND_BRIGHT = "\033[0;101m";// RED
+        public static final String GREEN_BACKGROUND_BRIGHT = "\033[0;102m";// GREEN
+        public static final String YELLOW_BACKGROUND_BRIGHT = "\033[0;103m";// YELLOW
+        public static final String BLUE_BACKGROUND_BRIGHT = "\033[0;104m";// BLUE
+        public static final String PURPLE_BACKGROUND_BRIGHT = "\033[0;105m"; // PURPLE
+        public static final String CYAN_BACKGROUND_BRIGHT = "\033[0;106m";  // CYAN
+        public static final String WHITE_BACKGROUND_BRIGHT = "\033[0;107m";   // WHITE
+    }
+
     Navire[][] tableau = new Navire[15][15];
     Cuirasse cuirasse;
     ArrayList<Croiseur> listCroiseur = new ArrayList<Croiseur>();
@@ -79,54 +154,54 @@ public class Grille implements Serializable {
     }
 
     public void dessiner() {
-        System.out.println("   | A | B | C | D | E | F | G | H | I | J | K | L | M | N | O |");
-        System.out.println("=================================================================");
+        System.out.println(ConsoleColors.BLACK_BOLD +"   | A | B | C | D | E | F | G | H | I | J | K | L | M | N | O |");
+        System.out.println("=================================================================" + ConsoleColors.RESET);
 
         for (int i = 0; i < 15; i++) {
-            System.out.print(cons[i] + "|");
+            System.out.print(ConsoleColors.BLACK_BOLD + cons[i] + "|"+ ConsoleColors.RESET);
             for (int j = 0; j < 15; j++) {
                 if (tableau[j][i] != null) {
                     int temp = casetouche(tableau[j][i], i, j);
                     if (temp == 1) {
-                        System.out.print(" X  ");
+                        System.out.print(ConsoleColors.RED_BOLD + " X  " +ConsoleColors.RESET);
                     } else {
-                        System.out.print(" " + tableau[j][i].getId() + "  ");
+                        System.out.print(" " + tableau[j][i].getId() + "  " );
                     }
                 } else {
-                    System.out.print("--- ");
+                    System.out.print(ConsoleColors.BLUE_BOLD+"--- "+ConsoleColors.RESET);
                 }
             }
-            System.out.println("|");
+            System.out.println(ConsoleColors.BLACK_BOLD+"|");
         }
-        System.out.println("=================================================================");
+        System.out.println("=================================================================" + ConsoleColors.RESET);
 
     }
 
     public void dessinerenemi() {
-        System.out.println("   | A | B | C | D | E | F | G | H | I | J | K | L | M | N | O |");
+        System.out.println(ConsoleColors.BLACK_BOLD +"   | A | B | C | D | E | F | G | H | I | J | K | L | M | N | O |");
         System.out.println("=================================================================");
         for (int i = 0; i < 15; i++) {
-            System.out.print(cons[i] + "|");
+            System.out.print(cons[i] + "|"+ConsoleColors.RESET);
             for (int j = 0; j < 15; j++) {
                 if (tableau[j][i] != null) {
                     int temp = casetouche(tableau[j][i], i, j);
                     if (temp == 1) {
-                        System.out.print("000 ");
+                        System.out.print(ConsoleColors.RED_BOLD +"000 "+ ConsoleColors.RESET);
                     }
                     else if(temp == 2){
                         System.out.print(" " + tableau[j][i].getId() + "  ");
                     }
                     else
                     {
-                        System.out.print("--- ");
+                        System.out.print(ConsoleColors.BLUE_BOLD+"--- "+ConsoleColors.RESET);
                     }
                 } else {
-                    System.out.print("--- ");
+                    System.out.print(ConsoleColors.BLUE_BOLD+"--- "+ ConsoleColors.RESET);
                 }
             }
-            System.out.println("|");
+            System.out.println(ConsoleColors.BLACK_BOLD+"|");
         }
-        System.out.println("=================================================================");
+        System.out.println("================================================================="+ConsoleColors.RESET);
     }
 
     public void setPositionRandom(Navire n) {
@@ -447,5 +522,6 @@ public class Grille implements Serializable {
             }
         }
     }
+
 
 }
